33906a61f002493d9efa37a28b49f5b95610c8fb
Jung Ian <jay2u8809@gmail.com>
2021-01-02 00:03:31 +0900

    Fix. Multi Lang JS

--- resources/js/custom/changeUrl.js
+++ resources/js/custom/changeUrl.js
@@ -8,14 +8,14 @@ $(function() {
 
     if (koUrl !== undefined || jpUrl !== undefined) {
         
-        let currentHostName = location.hostname;
+        let currentUrlOrigin = location.origin;
         let currentPathName = location.pathname;
         let hasJp = currentPathName.indexOf('/jp/') != -1;
         koUrl.setAttribute('href',  hasJp
-                                    ? currentHostName + currentPathName.slice(3, currentPathName.length)
+                                    ? currentUrlOrigin + currentPathName.slice(3, currentPathName.length)
                                     : location.href);
         jpUrl.setAttribute('href',  hasJp
                                     ? location.href
-                                    : currentHostName + '/jp' + currentPathName);
+                                    : currentUrlOrigin + '/jp' + currentPathName);
     }
 });
